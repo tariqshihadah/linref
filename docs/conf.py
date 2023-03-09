491a0c5d411b51640826751af679bc8f2df52f42
Tariq Shihadah <tariq.shihadah@gmail.com>
2023-03-09 09:29:43 -0600

    Change to logo only

--- docs/conf.py
+++ docs/conf.py
@@ -35,3 +35,7 @@ source_suffix = {
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = 'source/Linref_Logo_Simple_AllWhite.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True
+}
