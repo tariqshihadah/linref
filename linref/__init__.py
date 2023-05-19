d711338885c3dd5cfe19ba305dd4e1f75da477d8
tariqshihadah <tariq.shihadah@gmail.com>
2023-05-19 15:23:56 -0500

    Add package-level access to generate_linear_events function

--- linref/__init__.py
+++ linref/__init__.py
@@ -2,3 +2,4 @@ from linref.events.collection import EventsCollection, EventsGroup, from_standar
 from linref.events.merge import EventsMerge, EventsMergeAttribute, EventsMergeTrace
 from linref.events.union import EventsUnion
 from linref.route import MLSRoute, combine_mpgs
+from linref.events.synthesis import generate_linear_events
