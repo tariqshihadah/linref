b3a92b39aeffcebb53616b0253a6d16eae54a3e1
tariqshihadah <tshihadah@tooledesign.com>
2026-04-20 17:51:15 -0500

    factor LRS class out of ext/base.py

--- linref/ext/__init__.py
+++ linref/ext/__init__.py
@@ -1,5 +1,6 @@
 from linref.ext.spatial import parallel_project_hausdorff
-from linref.ext.base import parse_geoms_m_shapely, parse_geoms_m_wkt, LRS, LRS_Accessor
+from linref.ext.lrs import LRS
+from linref.ext.base import parse_geoms_m_shapely, parse_geoms_m_wkt, LRS_Accessor
 
 __all__ = [
     'parallel_project_hausdorff',
